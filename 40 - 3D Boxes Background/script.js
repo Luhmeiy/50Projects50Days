2941daae2c1f9c68debe0d532bee78f43b051b28
Luhmeiy <Medeiros.luizhenrique@yahoo.com.br>
2022-09-02 16:39:22 -0300

    Small Fix on Code #40

--- 40 - 3D Boxes Background/script.js
+++ 40 - 3D Boxes Background/script.js
@@ -4,7 +4,6 @@ let   row   = 0;
 let   col   = 0;
 
 while (row != 4 && col != 4) {
-    console.log(row, col)
     boxes.innerHTML += `<div class="box" style="background-position: ${col * -125}px ${row * -125}px;"></div>`;
     col++;
     if (col != 0 && col % 4 == 0) changeRow();
@@ -15,4 +14,4 @@ function changeRow() {
     col = 0;
 }
 
-magic.addEventListener('click', () => boxes.classList.toggle("join"))
+magic.addEventListener('click', () => boxes.classList.toggle("join"));
